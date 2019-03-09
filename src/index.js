250475b9c07dff36399eb811c3a61b961f121780
Dan Morgan <dannydev@icloud.com>
2019-03-09 18:26:50 -0500

    Updated post downloads

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
+require("dotenv").config();
+
 const execa = require("execa");
 const Listr = require("listr");
-const config = require("./setup/config");
-const clean = require("./setup/clean");
 
-require("dotenv").config();
+const testConfig = require("./setup/test-config");
+const cleanDist = require("./setup/clean-dist");
+const downloadPosts = require("./wordpress/download-posts");
+const transformPosts = require("./wordpress/transform-posts");
 
 const tasks = new Listr([
   {
     title: "Test Environment Config",
-    task: () => config()
+    task: () => testConfig()
   },
   {
     title: "Clean Destination Folder",
-    task: () => clean()
+    task: () => cleanDist()
+  },
+  {
+    title: "Get WordPress Posts",
+    task: () => {
+      return new Listr([
+        {
+          title: "Download raw JSON",
+          task: () => downloadPosts()
+        },
+        {
+          title: "Transform into Contentful format",
+          task: () => transformPosts()
+        }
+      ]);
+    }
   }
 ]);
 
-tasks.run().catch(err => {
-  console.error(err);
-});
+tasks.run().catch(err => console.error(err));
