2cafda4f99d78c0a9c72c7d38779c6f7ac7d6515
Dan Morgan <dannydev@icloud.com>
2019-03-10 15:05:58 -0400

    Create list of assets from folder of post data

---
+++ src/wordpress/create-asset-list.js
@@ -0,0 +1,61 @@
+const fetch = require("node-fetch");
+const fs = require("fs-extra");
+const path = require("path");
+const { Observable } = require("rxjs");
+const {
+  ASSET_DIR_LIST,
+  POST_DIR_TRANSFORMED,
+  MOCK_OBSERVER,
+  findByGlob
+} = require("../util");
+
+const urlById = (url, id) => `${url}/media/${id}`;
+
+const listOfImagesByPost = async (post, url) => {
+  const images = [];
+  if (post.featured_media) {
+    const postId = post.id;
+    const mediaNumber = post.featured_media;
+    const response = await fetch(urlById(url, mediaNumber));
+    const { status } = response;
+    // Save data and move on to the next page
+    if (status === 200) {
+      const json = await response.json();
+      images.push({
+        mediaNumber,
+        link: json.guid.rendered,
+        title: json.title.rendered || "",
+        description: json.alt_text || "",
+        postId
+      });
+    }
+  }
+  return images.concat(post.bodyImages ? post.bodyImages : []);
+};
+
+const assets = async (url, observer = MOCK_OBSERVER) => {
+  await fs.ensureDir(ASSET_DIR_LIST);
+  const files = await findByGlob("*.json", { cwd: POST_DIR_TRANSFORMED });
+  observer.next(`Processing ${files.length} posts`);
+  const queue = [...files];
+  let list = [];
+  while (queue.length) {
+    const file = queue.shift();
+    const post = await fs.readJson(path.join(POST_DIR_TRANSFORMED, file));
+    const images = await listOfImagesByPost(post, url);
+    list = list.concat(images);
+    observer.next(
+      `Processed ${list.length} images. (${files.length - queue.length} / ${
+        files.length
+      } posts)`
+    );
+  }
+
+  await fs.writeJson(path.join(ASSET_DIR_LIST, "assets.json"), list, {
+    spaces: 2
+  });
+  observer.complete();
+};
+
+module.exports = () =>
+  new Observable(observer => assets(process.env.API_URL, observer));
--- src/wordpress/user-download.js
+++ src/wordpress/user-download.js
@@ -2,19 +2,17 @@ const fetch = require("node-fetch");
 const fs = require("fs-extra");
 const path = require("path");
 const { Observable } = require("rxjs");
-
-const MOCK_OBSERVER = { next: console.log, complete: console.success };
-const DEST_DIR = path.join(process.cwd(), "dist", "users-raw-by-page");
+const { USER_DIR_ORIGINALS, MOCK_OBSERVER } = require("../util");
 
 const users = async (url, observer = MOCK_OBSERVER) => {
-  await fs.ensureDir(DEST_DIR);
+  await fs.ensureDir(USER_DIR_ORIGINALS);
   const response = await fetch(`${url}/users`);
   const { status } = response;
   // Save data and move on to the next page
   if (status === 200) {
     const json = await response.json();
-    const dest = path.join(DEST_DIR, `users.json`);
-    await fs.writeJson(dest, json);
+    const dest = path.join(USER_DIR_ORIGINALS, `users.json`);
+    await fs.writeJson(dest, json, { spaces: 2 });
     return observer.complete();
   }
   throw new Error(response);
